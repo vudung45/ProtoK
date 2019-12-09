ede153277a213d8052ebe24a52d05963a8be5920
David Prasse <dprasse@utexas.edu>
2019-12-08 23:58:39 -0600

    Added POST/GET support for adding new functions.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const fs = require('fs')
+const util = require('util')
+
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -25,8 +28,41 @@ app.post('/create', (req, res) => {
   const dependencies = req.body.dependencies;
   const language = req.body.language;
 
-  res.send(func + ' ' + dependencies + ' ' + language);
+  let combined_vals = {"function": func, "dependencies": dependencies, "language": language};
+
+  try {
+    let readF = fs.readFileSync('./temp.json', 'utf-8');
+    let parsed = JSON.parse(readF);
+    parsed.entries.push(combined_vals);
+  
+    fs.writeFile('./temp.json', JSON.stringify(parsed), function (err) {
+  
+    });
+  } catch (error) {
+    // This error thrown is likely because we tried parsing (converting to JSON) an empty file
+    // In this case, we just create the correct json file with the new entry given
+    let parsed = JSON.parse('{"entries":[]}');
+    parsed.entries.push(combined_vals);
+
+    fs.writeFile('./temp.json', JSON.stringify(parsed), function (err) {
+  
+    });
+    console.log(error);
+  }
+
+  res.send(combined_vals);
 });
 
+app.get('/readdata', (req, res) => {
+
+  try {
+    let rawdata = fs.readFileSync('./temp.json');
+    let parsed = JSON.parse(rawdata);
+    res.send(parsed);
+  } catch (error) {
+    res.send("Unable to load data. Make sure that temp.json has data in it.");
+  }
+
+});
 
 app.listen(port, () => console.log('We are live on Port:' + port));
